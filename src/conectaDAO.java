5569290ee287235e0c7e47e23d3d01c4d13e5d5a
haech <biancardwz@gmail.com>
2026-03-14 17:07:05 -0300

    Ajuste de senha e teste de cadastro ok

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+conn = DriverManager.getConnection("jdbc:mysql://localhost/leiloes_db?user=root&password=95153695");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
