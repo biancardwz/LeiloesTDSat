884d98fe193645b5aedf7d260180b1a0a252d576
haech <biancardwz@gmail.com>
2026-03-19 15:24:28 -0300

    Commit #2: Implementada a funcionalidade venderProduto na tela de listagem

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -137,13 +137,18 @@ public class listagemVIEW extends javax.swing.JFrame {
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
         String id = id_produto_venda.getText();
-        
+    
+    if (!id.isEmpty()) {
         ProdutosDAO produtosdao = new ProdutosDAO();
         
-        //produtosdao.venderProduto(Integer.parseInt(id));
+        produtosdao.venderProduto(Integer.parseInt(id));
+        
+        id_produto_venda.setText("");
         listarProdutos();
+    } else {
+        javax.swing.JOptionPane.showMessageDialog(null, "Informe o ID do produto para vender.");
     }//GEN-LAST:event_btnVenderActionPerformed
-
+    }
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
         //vendasVIEW vendas = new vendasVIEW(); 
         //vendas.setVisible(true);
